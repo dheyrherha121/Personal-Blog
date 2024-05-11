fddaca8df29acffb826b8bd0cb2a04a7564290d9
Akere Aishat <mojiradeaisha850@gmail.com>
2024-05-12 00:38:34 +0100

    CORS update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from .crud import articles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(articles.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def index():
